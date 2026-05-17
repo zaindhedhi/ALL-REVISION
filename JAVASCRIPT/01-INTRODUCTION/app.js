852614665d71906e36675eb87d31bdc606f491da
zaindhedhi <zaindhedhi@oulook.com>
2026-05-17 22:28:53 +0500

    variables

---
+++ JAVASCRIPT/01-INTRODUCTION/app.js
@@ -0,0 +1,54 @@
+console.log("HELLO WORLD!");
+
+console.log(2+2);
+
+console.log(2);
+
+console.log(10/5);
+
+console.log("string "+"hello javascript");
+
+console.log("concatenation ", "2" + "2"); // concatenation
+
+console.log("2"- "2");
+
+console.log("numbers ==> ", 2+2);
+
+console.log("boolean => " + true);
+
+
+// data types
+
+// string "hgahgwsh33635hh$$$llekkei"
+// numbers 5352772.9936
+// boolean true | false 
+
+// alert("zain");
+// alert(2+2);
+
+// variables
+
+// var city = "karachi";
+// console.log(city);
+
+var username = "zain";
+var age = "20";
+var isLoggedIn = true;
+
+console.log(username , age , isLoggedIn);
+
+var fullName = "zain dhedhi";
+var fullname = "zain yousuf";
+console.log(fullName);
+console.log(fullname);
+
+// var username;
+// console.log(username);
+
+var city = "karachi";  // declaration
+// var city = "islamabad"; // redeclare
+// city = "lahore"; // reassign
+console.log(city);
+
+
+ 
