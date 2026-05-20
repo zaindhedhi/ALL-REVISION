00b3aa3183e3afb13b296ebc6a315bcc4a27540b
zaindhedhi <zaindhedhi@oulook.com>
2026-05-20 13:38:07 +0500

    airthmatic & assignment operator completed

--- JAVASCRIPT/02-operation&expression/app.js
+++ JAVASCRIPT/02-operation&expression/app.js
@@ -1,3 +1,36 @@
 console.log("hello world");
 
-// alert('welcome to js land ')
+// alert('welcome to js land\n happy coding ');
+
+// Airthmatic operator 
+
+// /
+// *
+// +
+// -
+// % modulus remainder bata ta hai division ka 
+
+// var num1 = 30;
+// var num2 = 20;
+
+// console.log(num1 / num2);
+// console.log(num1 * num2);
+// console.log(num1 + num2);
+// console.log(num1 -  num2);
+// console.log(num1 % num2);
+
+// Assignment operator
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+// var num1 = 20;
+// num1 += 30;
+// num1 -= 30;
+// num1 *= 2;
+// num1 /= 4;
+
+// console.log(num1);
