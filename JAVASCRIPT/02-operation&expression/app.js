7eaaa8b985c55d38881d93fac2212236df3482d4
zaindhedhi <zaindhedhi@oulook.com>
2026-05-21 13:09:31 +0500

    comparision operator done

--- JAVASCRIPT/02-operation&expression/app.js
+++ JAVASCRIPT/02-operation&expression/app.js
@@ -34,3 +34,28 @@ console.log("hello world");
 // num1 /= 4;
 
 // console.log(num1);
+
+// comparision operator 
+// comparision operator hamesha boolean return kry ga 
+
+// var num1 = 30;
+// var num2 = 20;
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+
+// var comparison = num1 == num2;
+// console.log(comparison);
+
+// console.log(20 == "20"); // == data type check nhi krta 
+
+// console.log(20 === "20"); // === is ny data type check ki hai
+
+// console.log(20 != "20"); // != data type check nhi krta 
+// console.log(20 != 30);
+
+// console.log(20 !== "20"); // !== ye data type check krta hai
+
+// console.log(20 >= 20); // 20 bara hai ya barabr hai 20 sy
+// console.log(20 <= 20); // 20 chota hai ya barabr hai 20 ky
+
